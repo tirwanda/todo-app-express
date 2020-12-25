3c75dd67524b62d4fd9d510ef43c60ec9b1600dd
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2020-12-25 20:41:02 +0700

    Adding Delete Function

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,7 +3,7 @@ let data = [{item: 'Get milk'}, {item: 'Kick some coding ass'}];
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
-    app.get('/todo', (req, res) => {
+    app.get('/', (req, res) => {
         res.render('todo', {todos: data});
     });
 
@@ -12,7 +12,10 @@ module.exports = function(app) {
         res.json(data);
     });
 
-    app.delete('/todo', (req, res) => {
-
+    app.delete('/todo/:item', (req, res) => {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 };
